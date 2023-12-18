83b80e3d3faa17a3f966c060bedd0523f397652b
KAU5H4L <pacharne.ai21@bmsce.ac.in>
2023-12-18 15:08:33 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+label_mapping = {
+    'A': '4',
+    'B': '13',
+    'E': '3',
+    'G': '6',
+    'H': '#',
+    'I': '1',
+    'O': '0',
+    'R': '12',
+    'S': '5',
+    'T': '+',
+}
+
+
+def conv(label_mapping, x):
+    result = ''
+    for i in x:
+        result += label_mapping.get(i, i)
+
+    return result
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        input_text = input_text.upper()
+        output_text = conv(label_mapping, input_text)
+        return render_template('index.html', output_text=output_text)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
