7f59a931d21990273385d4cac34043f4e8e1fe4d
Pranshu <pranshuarora1618@gmail.com>
2024-04-08 12:05:15 +0530

    fuddu hosting

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Store messages uploaded by users
 messages = []
 
-# Encoding mapping
 label_mapping = {
     'A': '4',
     'B': '13',
@@ -19,14 +17,11 @@ label_mapping = {
     'T': '+',
 }
 
-# Decoding mapping (reverse of encoding mapping)
 reverse_mapping = {value: key for key, value in label_mapping.items()}
 
-# Function to encode a message
 def encode_message(text):
     return ''.join(label_mapping.get(char.upper(), char) for char in text)
 
-# Function to decode a message
 def decode_message(text):
     return ''.join(reverse_mapping.get(char, char) for char in text)
 
